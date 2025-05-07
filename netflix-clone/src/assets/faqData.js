8bd20e5daa1c53bd0e59d1368a3c7529b1789ce8
AMETIY <amanuelaraya908@gmail.com>
2025-05-06 18:52:39 -0600

    feat:Added FAQ Data

---
+++ netflix-clone/src/assets/faqData.js
@@ -0,0 +1,24 @@
+const faqData = [
+    {
+      question: "What is Netflix?",
+      answer: "Netflix is a streaming service offering a wide variety of TV shows, movies, anime, documentaries, and more."
+    },
+    {
+      question: "How much does Netflix cost?",
+      answer: "Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device for one fixed monthly fee."
+    },
+    {
+      question: "Where can I watch?",
+      answer: "Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web or on your device."
+    },
+    {
+      question: "How do I cancel?",
+      answer: "Netflix is flexible. There are no annoying contracts and no commitments. Cancel online anytime."
+    },
+    {
+      question: "What can I watch on Netflix?",
+      answer: "Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more."
+    }
+  ];
+
+  export default faqData
