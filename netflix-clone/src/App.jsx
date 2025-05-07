05b6fa83309d6099216dc247fc7929b94c701779
AMETIY <amanuelaraya908@gmail.com>
2025-05-06 20:37:24 -0600

    chore: finalize app before production build

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import './app.css'
-import Home from './pages/Home/Home'
+import React from "react";
+import "./app.css";
+import Home from "./pages/Home/Home";
 
 const App = () => {
   return (
     <>
-<Home />
+      <Home />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
