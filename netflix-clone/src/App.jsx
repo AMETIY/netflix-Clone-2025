d68b3c1481c42be08f4bd7c7f0dbecc5516e7147
AMETIY <amanuelaraya908@gmail.com>
2025-05-06 19:01:42 -0600

    refactor: clean up code and prepare for build

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import './app.css'
+import Home from './pages/Home/Home'
 
 const App = () => {
   return (
     <>
-
+<Home />
     </>
   )
 }
--- netflix-clone/src/Components/Header/Header.jsx
+++ netflix-clone/src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav, Button, NavDropdown } from "react-bootstrap";
 
 const Header = () => {
   return (
@@ -43,12 +43,12 @@ const Header = () => {
             <img src={NetflixLogo} alt="Netflix logo" width="100" />
           </Navbar.Brand>
 
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle className="var" aria-controls="navbarScroll" />
 
-          <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Collapse id="navbarScroll">
             <div className="d-flex justify-content-between w-100 align-items-center">
               {/* Left Nav */}
-              <Nav className="gap-3">
+              <Nav className="gap-3 " style={{ maxHeight: '100px' }}  navbarScroll>
                 <Nav.Link href="#">Home</Nav.Link>
                 <Nav.Link href="#">Tv Shows</Nav.Link>
                 <Nav.Link href="#">Movies</Nav.Link>
@@ -58,12 +58,18 @@ const Header = () => {
               </Nav>
 
               {/* Right Icons */}
-              <Nav className="gap-3 header-icons">
+              <Nav className="gap-3 header-icons " navbarScroll>
                 <Nav.Link href="#"><SearchIcon /></Nav.Link>
                 <Nav.Link href="#"><NotificationsIcon /></Nav.Link>
                 <Nav.Link href="#"><AccountBoxIcon /></Nav.Link>
-                <Nav.Link href="#"><ArrowDropDownIcon /></Nav.Link>
+                {/* <Nav.Link href="#"><ArrowDropDownIcon /></Nav.Link> */}
+                <NavDropdown title="Lanuage" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">English</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Amharic</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              </NavDropdown>
               </Nav>
+      <Button className="nav-button" variant="outline-danger">Logout</Button>
             </div>
           </Navbar.Collapse>
         </Container>
